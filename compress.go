13e18f676fe0b5c0b05cd06cf0cb3d33a24d3782
Armando G. Mondul <mondul@huyzona.com>
2024-03-18 17:46:01 -0500

    Update compressed hosts
    Also fixed compression to avoid EOF errors

--- compress.go
+++ compress.go
@@ -4,17 +4,15 @@ import (
 	"bytes"
 	"compress/flate"
 	"fmt"
-	"io"
 	"os"
 )
 
 func compress(file_path string) {
-	file, err := os.Open(file_path)
+	file_contents, err := os.ReadFile(file_path)
 	if err != nil {
 		fmt.Printf("! Could not load file '%s': %s\n", file_path, err)
 		os.Exit(1)
 	}
-	defer file.Close()
 
 	var buf bytes.Buffer
 	flate_writer, err := flate.NewWriter(&buf, flate.BestCompression)
@@ -22,10 +20,9 @@ func compress(file_path string) {
 		fmt.Printf("! Could not create compressor: %s\n", err)
 		os.Exit(1)
 	}
-	defer flate_writer.Close()
 
-	io.Copy(flate_writer, file)
-	flate_writer.Flush()
+	flate_writer.Write(file_contents)
+	flate_writer.Close()
 
 	for i, byte := range buf.Bytes() {
 		fmt.Printf(" 0x%02X,", byte)
